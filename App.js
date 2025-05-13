5b9d47775013205476f730d4b8bfc2f8ad612316
Ayush Kumar Jha <kumarayushjha123@gmail.com>
2025-05-13 11:33:21 +0530

    First commit

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const App = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView 
+        source={{ uri: 'https://healthcare-d7vz.onrender.com/' }}
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+    marginTop: 17,
+  },
+});
+
+export default App;
